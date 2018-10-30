ca3998edb8dbce6b3833e674d9762a926474579f
Jee Hoon <yamoo9@naver.com>
2018-10-30 17:56:29 +0900

    PostView 컴포넌트 분리 및 활용

---
+++ client/components/Posts/PostPreview.vue
@@ -0,0 +1,86 @@
+<template lang="pug">
+article.post-preview
+	nuxt-link(:to="'/posts/' + id")
+		figure.post-thumbnail(:style="{'background-image': 'url('+ thumbnail +')'}")
+		.post-content
+			h2.post-title {{ title }}
+			p.post-text {{ ellipseContent }}
+</template>
+
+<script>
+export default {
+  name: 'PostPreview',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    thumbnail: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    ellipseContent() {
+      const ellipseText =
+        this.content.length < 65
+          ? this.content
+          : this.content.slice(0, 60) + '...'
+      return ellipseText
+    }
+  }
+}
+</script>
+
+
+<style lang="sass" scoped>
+.post-preview
+  border: 1px solid rgba(#aaa, 0.45)
+  box-shadow: 0 2px 2px rgba(#aaa, 0.45)
+  background-color: #fff
+  flex: 1 0 90%
+  margin:
+    left: 5%
+    right: 5%
+    bottom: 15px
+
+a
+  text-decoration: none
+  color: darken(#5db4f3, 20%)
+  transition: all 0.4s ease
+  &:hover .post-content,
+  &:active .post-content
+    background-color: rgba(#5db4f3, 0.26)
+
+.post-thumbnail
+  width: 100%
+  height: 150px
+  margin: 0
+  background-position: center
+  background-size: cover
+
+.post-title
+  margin-bottom: 10px
+
+.post-text
+  margin-top: 0
+
+.post-content
+  padding: 10px
+  text-align: center
+
+@media (min-width: 768px)
+  .post-preview
+    flex: 0 0 45%
+    margin:
+      left: 10px
+      right: 10px
+</style>
