9b1c3391da6f88fc51c34f858d76a91671c81cf3
Jee Hoon <yamoo9@naver.com>
2018-11-01 16:27:55 +0900

    인증 페이지

---
+++ client/pages/admin/auth/index.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+.page-admin-auth.container
+  form.auth-form
+    ui-input(
+      type="email",
+      uniqueId="email") 이메일 주소
+    ui-input(
+      type="password",
+      uniqueId="password") 비밀번호
+    .button-group(role="group")
+      ui-button(style="margin-right: 10px")
+        | {{ isLogin ? '로그인' : '회원가입' }}
+      ui-button(
+        type="button",
+        styles="inverted",
+        @click="isLogin = !isLogin"
+      )
+        | {{ '"' + (isLogin ? '회원가입' : '로그인') + '" 폼으로 변경' }}
+</template>
+
+<script>
+import UiInput from '@/components/UI/UiInput'
+import UiButton from '@/components/UI/UiButton'
+
+export default {
+  components: { UiInput, UiButton },
+  data() {
+    return {
+      isLogin: true
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.auth-form
+  width: 80%
+  margin:
+    left: auto
+    right: auto
+
+.button-group
+  margin-top: 30px
+</style>
