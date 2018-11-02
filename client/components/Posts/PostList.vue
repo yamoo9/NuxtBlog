d7c3315ca0bc6ec856258767432dfcf88f23e31a
Jee Hoon <yamoo9@naver.com>
2018-11-02 13:59:34 +0900

    비동기 시뮬레이션(지연된 렌더링) with asyncData

--- client/components/Posts/PostList.vue
+++ client/components/Posts/PostList.vue
@@ -1,19 +1,14 @@
 <template lang="pug">
 section.post-list
-	post-preview(
-		id="1",
-		:isAdmin="isAdmin"
-		title="빅 데이터",
-		thumbnail="//goo.gl/mJ5Vsy",
-		content="빅 데이터란? 기존 데이터베이스 관리도구의 능력을 넘어서는 대량의 정형 또는 비정형 데이터 집합을 포함한 데이터로부터 가치를 추출하고 결과를 분석하는 기술이다."
-	)
-	post-preview(
-		id="2",
-		:isAdmin="isAdmin"
-		title="머신 러닝",
-		thumbnail="//goo.gl/HoiVkE",
-		content="머신 러닝은 인공 지능의 한 분야로, 컴퓨터가 학습할 수 있도록 하는 알고리즘과 기술을 개발하는 분야를 말한다."
-	)
+  post-preview(
+    v-for="post in posts",
+    :key="post.id",
+    :id="post.id",
+    :isAdmin="isAdmin",
+    :title="post.title",
+    :thumbnail="post.thumbnail",
+    :content="post.content"
+  )
 </template>
 
 <script>
@@ -25,6 +20,10 @@ export default {
     isAdmin: {
       type: Boolean,
       default: false
+    },
+    posts: {
+      type: Array,
+      required: true
     }
   }
 }
--- client/pages/posts/_id/index.vue
+++ client/pages/posts/_id/index.vue
@@ -1,62 +1,96 @@
 <template lang="pug">
-	.page-single-post
-		section.post
-			h1.post-title 포스트 제목
-			.post-info
-				span.post-data 작성일자
-				span.post-author 작성자
-			p.post-content
-				| 포스트 내용
-		section.post-feedback
-			p
-				| 해당 포스트에 대해 궁금한 점이 있으면
-				a(href="mailto:yamoo9@naver.com") yamoo9@naver.com
-				| 로 문의주세요.
+  .page-single-post
+    section.post
+      h1.post-title {{ loadedPost.title }}
+      .post-info
+        span.post-author {{ loadedPost.author }}
+        span.post-data {{ loadedPost.updatedDate }}
+      img.post-image(:src="loadedPost.thumbnail", alt, width="600")
+      p.post-content {{ loadedPost.content }}
+    section.post-feedback
+      p
+        | 해당 포스트에 대해 궁금한 점이 있으면
+        a(href="mailto:yamoo9@naver.com") yamoo9@naver.com
+        | 로 문의주세요.
 </template>
 
+<script>
+export default {
+  asyncData({ route }, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPost: {
+          id: '1',
+          title: '빅 데이터',
+          author: '야무(yamoo9)',
+          updatedDate: new Date().toLocaleString(),
+          thumbnail: '//goo.gl/mJ5Vsy',
+          content:
+            '빅 데이터란? 기존 데이터베이스 관리도구의 능력을 넘어서는 대량의 정형 또는 비정형 데이터 집합을 포함한 데이터로부터 가치를 추출하고 결과를 분석하는 기술이다.'
+        }
+      })
+    }, 1000)
+  }
+}
+</script>
+
+
 <style lang="sass" scoped>
-	.page-single-post
-		padding: 30px
-		text-align: center
-		box-sizing: border-box
+  .page-single-post
+    padding: 30px
+    text-align: center
+    box-sizing: border-box
 
-	.post
-		width: 100%
+  .post
+    width: 100%
 
-	.post-title
-		margin: 0
+  .post-title
+    margin: 0
 
-	.post-info
-		display: flex
-		justify-content: center
-		align-items: center
-		flex-direction: column
-		box-sizing: inherit
-		border-bottom: 1px solid #ccc
-		padding: 10px
+  .post-info
+    display: flex
+    justify-content: center
+    align-items: center
+    flex-direction: column
+    box-sizing: inherit
+    border-bottom: 1px solid #ccc
+    padding: 10px
 
-	.post-data,
-	.post-author
-		margin: 0 10px
-		color: #9a9a9a
+  .post-data,
+  .post-author
+    margin: 0 10px
+    color: #9a9a9a
 
-	.post-feedback a
-		padding:
-			left: 5px
-			right: 5px
-		color: #46b784
-		text-decoration: none
-		transition: color 0.4s ease
-		&:hover,
-		&:active
-			color: darken(#46b784, 20%)
+  .post-image
+    margin-top: 20px
+    width: 100%
+    height: auto
+
+  .post-feedback
+    width: 60%
+    margin:
+      top: 30px
+      right: auto
+      left: auto
+    border-top: 1px solid rgba(#000, 0.1)
+    padding-top: 15px
+    a
+      padding:
+        left: 5px
+        right: 5px
+      color: #46b784
+      text-decoration: none
+      transition: color 0.4s ease
+      &:hover,
+      &:active
+        color: darken(#46b784, 20%)
 
 
-	@media (min-width: 768px)
-		.post
-			width: 600px
-			margin: auto
+  @media (min-width: 768px)
+    .post
+      width: 600px
+      margin: auto
 
-		.post-info
-			flex-direction: row
+    .post-info
+      flex-direction: row
 </style>
